7cac15bdc4b5e5ce4c9fb0af2eaa390fa48833a7
Lars Haulin <lars@haulin.se>
2016-10-28 03:27:29 +0200

    Added more info on structure, can now make a basic print of level in ascii

--- decoder.py
+++ decoder.py
@@ -1,15 +1,6 @@
 import qrtools, os
 
-
-def openLevel(imagename):
-	qr = qrtools.QR()
-	assert(qr.decode('cargo.png'))
-	levelData = eval(repr(qr.data)[1:])
-
-	header = levelData[:4]
-	print 'Header:', header.encode('hex')
-
-	compressedData = levelData[4:]
+def uncompress(compressedData):
 	f = open('in.tmp','w')
 	f.write(compressedData);
 	f.close()
@@ -18,16 +9,36 @@ def openLevel(imagename):
 	[l, data] = indata.split('\n',1)
 	l = int(l)
 	assert l == len(data)
+	return data
+
+def openLevel(imagename):
+	qr = qrtools.QR()
+	assert(qr.decode(imagename))
+	levelData = eval(repr(qr.data)[1:])
+
+	header = levelData[:4]
+	#print 'Header:', header.encode('hex')
+
+	compressedData = levelData[4:]
+	data = uncompress(compressedData)
+
 	nameLen = ord(data[0])
 	name = data[1:1+nameLen]
-	print repr(name)
 	authorLen = ord(data[1+nameLen]) + 1
 	author = data[2+nameLen:1+nameLen+authorLen]
-	print repr(author)
 
 	levelData = data[1+nameLen+authorLen:]
-	print hex(len(levelData))
 	return name, author, levelData
 
-level = openLevel('cargo.png')
-print level
+level = openLevel('big.png')
+name, author, data = level
+print name, author
+
+# Printing only works if level does not contain multiple byte blocks
+assert len(data) == 0x1000, "level contains multibyte blocks!"
+
+for i in range(16):
+	for j in range(16):
+		a = i*16 + j*256
+		print data[a:a+16].encode('hex')
+	print
