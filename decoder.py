51a5b5648df7e1909dd87641868d9c3dfdd404c6
Lars Haulin <lars@haulin.se>
2016-10-28 20:32:51 +0200

    Can now create QR-codes and generate simple levels

--- decoder.py
+++ decoder.py
@@ -1,4 +1,17 @@
-import qrtools, os
+import qrtools, os, qrcode
+
+HEADER = '01130dfc'.decode('hex')
+
+def compress(compressedData):
+	f = open('in.tmp','w')
+	f.write(compressedData);
+	f.close()
+	assert(os.system('./compress %u < in.tmp > out.tmp'%len(compressedData)) == 0)
+	indata = open('out.tmp').read()
+	[l, data] = indata.split('\n',1)
+	l = int(l)
+	assert l == len(data)
+	return data
 
 def uncompress(compressedData):
 	f = open('in.tmp','w')
@@ -18,6 +31,7 @@ def openLevel(imagename):
 
 	header = levelData[:4]
 	#print 'Header:', header.encode('hex')
+	assert header == HEADER, repr((header, HEADER))
 
 	compressedData = levelData[4:]
 	data = uncompress(compressedData)
@@ -30,15 +44,29 @@ def openLevel(imagename):
 	levelData = data[1+nameLen+authorLen:]
 	return name, author, levelData
 
-level = openLevel('big.png')
-name, author, data = level
-print name, author
+def createQR(name, author, levelData):
+	rawData = chr(len(name)) + name + chr(len(author)) + author + levelData
+	data = HEADER + compress(rawData)
+	qrcode.make(data).show()
 
-# Printing only works if level does not contain multiple byte blocks
-assert len(data) == 0x1000, "level contains multibyte blocks!"
+def printLevel(data):
+	# Printing only works if level does not contain multiple byte blocks
+	assert len(data) == 0x1000, "level contains multibyte blocks!"
 
-for i in range(16):
-	for j in range(16):
-		a = i*16 + j*256
-		print data[a:a+16].encode('hex')
-	print
+	for i in range(16):
+		for j in range(16):
+			a = i*16 + j*256
+			print data[a:a+16].encode('hex')
+		print
+
+def testGeneration():
+	A = '\x01'*16
+	B = '\x01' + ('\x00'*14) + '\x01'
+	b = '\x01' + '\x0f\x04' + ('\x00'*13) + '\x01'
+	C = '\x00' * 16
+	D = A + B*14 + A
+	E = b + C*14 + B
+	F = D + E*12 + D + E + D
+	createQR('Big Stone Cube', 'LarsH', F)
+
+testGeneration()
